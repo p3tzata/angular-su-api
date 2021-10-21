560563263c2dabf4bff8e6f54f770c8542f08631
p3tzata <p3tzata_@abv.bg>
2021-10-21 11:37:39 +0300

    21.10

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 var express = require("express");
 var app = express();
 
-app.get("/url", (req, res, next) => {
- res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:4200');
+
+    next();
+});
+
+
+app.get("/moviesDb/popular", (req, res, next) => {
+	
+// res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+//res.json(req.query.fake_api_key);
+res.json({result:[
+	{id:"1",
+	image_url:"",
+	title:"title1",
+	release_date:"01.01.2021"},
+	{id:"2",
+	image_url:"",
+	title:"title2",
+	release_date:"01.01.2021"},
+	{id:"3",
+	image_url:"",
+	title:"title3",
+	release_date:"01.01.2021"}
+]});
+
 });
 
 
